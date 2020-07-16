e884aa1c0121e5cf66855e7a313cc592b3ad678f
spyrosgriniaris <spyros.gr21@gmail.com>
2020-07-16 16:00:28 +0300

    Initial Commit

---
+++ FootballApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+using System.Threading.Tasks;
+using FootballApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    //to controllerbase pou ginetai inherit den exei views, ta dinw egw apo angular
+    //to sketo controller exei,kai einai to mvc, enw egw ftiaxnw to api edw mono
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()  //iactionresult allows us to return http responses
+        {
+            //we want async method because our API must be able to handle multiple requests
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FootballApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using FootballApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+         public DataContext(DbContextOptions<DataContext> options) : base (options)
+         {}
+         
+         public DbSet<Value> Values {get; set;}
+    }
+}
